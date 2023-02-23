14cbc5f077c4f4a3650c33edc706ab8aa948adb9
Katsiaryna Sapunova <csapunova@gmail.com>
2023-02-23 14:49:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length%2!=0) {
+    return false;
+ }
+
+ let brackets = [];
+
+for(let i=0;i<bracketsConfig.length; i++){
+    brackets[i] = bracketsConfig[i][0] + bracketsConfig[i][1];
+}
+console.log(brackets);
+while(str.length>0){
+    let x = 0;
+    for (let i=0; i<brackets.length; i++){
+        if(str.includes(brackets[i]) ){
+            str = str.replace(brackets[i],'')
+        }else{
+            x++;
+        }
+    }
+    if(x === brackets.length){
+        return false;
+    }
+}
+
+return true;
 }
